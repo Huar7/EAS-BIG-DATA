f28812bad7011291164a96ca98035a4293b90961
Huar7 <theliq310@gmail.com>
2025-05-28 07:19:56 +0700

    pengubahan menjadi model Dict

--- main2.py
+++ main2.py
@@ -5,27 +5,33 @@ import pandas as pd
 def yf_first(isi: list):
     starter = yf.download(isi,period="5y", interval="1wk") 
     hasil = {}
+    once = 0
+    nulia = []
     for i in starter['Close']:
         julia = starter['Close'][i].dropna()
         nalia = []
         for kukukaka in range(len(julia)):
-            nalia.append({"Timestamp": julia.index[kukukaka].strftime("%Y/%m/%d"), "Close": julia[kukukaka].item()})
+            nalia.append(julia[kukukaka].item())
+            if once == 0:
+                nulia.append({"Timestamp":julia.index[kukukaka].strftime("%Y/%m/%d %X")})
+        once = 1
+
         hasil[i] = nalia 
-    return(hasil)
+    return(hasil, nulia)
 
 def data_ingest_run(isi: list):
     info = yf.Tickers(isi)
     hasil = {}
     try:
         historis = info.history(period='1d', interval='1m', progress=False, repair=True)
-        reynauld = historis.index[-1].strftime("%Y/%m/%d")
+        reynauld = {"Timestamp" : historis.index[-1].strftime("%Y/%m/%d %X")}
         for i in historis['Close']:
             dismas = historis['Close'][i].iloc[-1]
-            hasil[i] = [{"Timestamp": reynauld, "Close": dismas}]
+            hasil[i] = (dismas) # // perubahan pada output
+        return(hasil, reynauld)
     except:
         print("mencoba mengulang server")
         data_ingest_run(isi)
-    return(hasil)
 
 if __name__ == '__main__':
     indes = ["BTC-USD", "XRP-USD", "NVAX"]
--- spark_builder.py
+++ spark_builder.py
@@ -1,10 +1,12 @@
-import pyspark as psp
+from kafka.protocol.types import Schema
+import pyspark
 from pyspark.sql import SparkSession
 
 import kafka as kf
-import ast
 
 import json
+
+from yfinance import data
 
 # from multiprocessing import Process
 
@@ -21,6 +23,13 @@ def Rdata():
     )
     consumer.subscribe(topics=["stock_kotor"])
     once = 0
+    column = pyspark.sql.types.StructType([])
+
+    df = spark.createDataFrame(data=[], schema=column)
+
+    """
+    Pyspark hanya menerima dalam bentuk Dictionaries, sehingga kita harus membuat fungsi yang mengubah setiap nilai yang ada di dalam fungsi tersebut menjadi suatu dict
+    """
 
     while True:
         msg = consumer.poll(timeout_ms=1000)
@@ -29,13 +38,18 @@ def Rdata():
                 try:
                     # // FixMe: ini kenapa tetap str terus ya: ubah semua Timestamp menjadi dalam bentuk str
                     nilai_panggilan = value[0].value
+                    nilai_data = nilai_panggilan[0]
+                    chronos = nilai_panggilan[1]
                     print(f"nilai Panggilan {nilai_panggilan} {'=' * 50}")
 
                     if (
                         once == 0
                     ):  # // ini untuk mengatasi ketimpangan data awalan (Downloads)
-                        maxi = find_largest(nilai_panggilan)
+                        maxi = find_largest(nilai_data)
                         print(maxi)
+                        maxum = dataframe_normalization(
+                            nilai_data, len(nilai_data[maxi]), spark
+                        )
                     # -- NOTES;
 
                     once += 1
@@ -61,6 +75,26 @@ def find_largest(nil: dict) -> str:
     posisi = panjang.index(maxi)
     pos = naila[posisi]
     return pos
+
+
+def dataframe_normalization(data: dict, amount: int, sesi_spark: SparkSession):
+    """Amount adalah jumlah seharusnya dari data tersebut"""
+    junia = [*data]
+    for i in junia:
+        iterasi = amount - len(data[i])
+        match iterasi:
+            case 0:
+                pass
+            case _:
+                for _ in range(iterasi):
+                    data[i].append(
+                        data[i][-1]
+                    )  # // ini untuk menormalisasi data yang kosong dimana akan mengisi dengan nilai yang atas
+    to_dict = [dict(zip(data.keys(), values)) for values in zip(*data.values())]
+    print(to_dict)
+
+
+#   hasil = sesi_spark.createDataFrame()
 
 
 if __name__ == "__main__":
